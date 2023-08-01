1b3838c048859f2d42541b6b2fec9bb926a293da
abdyraman <abdyramanova.j@gmail.com>
2023-08-01 12:28:50 -0400

    app

--- Coropleth/Solved/app.py
+++ Coropleth/Solved/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from pymongo import MongoClient
 from credentials import username, password
+import json
 
 app = Flask(__name__)
 
@@ -30,6 +31,28 @@ def get_predictions_data():
         data.pop('_id', None)
 
     return jsonify(dataset)
+
+# Endpoint to retrieve the entire prediction dataset
+@app.route("/worldbank")
+def get_wbdata_data():
+    collection = db['world_bank_data']  
+
+    # Fetch all documents from the 'predictions' collection
+    dataset = list(collection.find({}))
+
+    # Convert the ObjectId to string and remove '_id' field for JSON serialization
+    for data in dataset:
+        data['_id'] = str(data['_id'])
+        data.pop('_id', None)
+
+    return jsonify(dataset)
+
+@app.route("/geoJsonData")
+def geojsonData(): 
+    file_path = 'data.geojson'
+    with open(file_path, 'r') as f: 
+        data = json.load(f)
+        return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
